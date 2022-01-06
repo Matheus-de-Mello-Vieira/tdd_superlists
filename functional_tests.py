03c3996a00c5a84239c241f46697b0938773cf0d
matheus <mellomatheus67@gmail.com>
2022-01-06 02:12:18 -0300

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -49,9 +49,10 @@ class NewVisitorTest(unittest.TestCase):
 
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_id('tr')
+        rows = table.find_elements_by_id('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not apper in table"
         )
         """
         Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro
